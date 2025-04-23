e313a827eb4a7aa349ddfbe3141affba98269f39
prathmeshkodag <prathmeshkodag51@gmail.com>
2025-04-23 23:05:35 +0530

    Uptd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,22 +20,22 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Follow Us</h3>
           <div className="social-icons">
-            <CiFacebook />
-            <FaInstagram />
-            <FaLinkedin />
-            <FaTwitterSquare />
-            <FaYoutube />
-            <SiQase />
+            <CiFacebook  size={40}/>
+            <FaInstagram size={40}/>
+            <FaLinkedin size={40}/>
+            <FaTwitterSquare size={40}/>
+            <FaYoutube size={40}/>
+            <SiQase size={40}/>
           </div>
         </div>
 
         <div className="footer-section">
           <h3>Head Office</h3>
           <ul className="office-info">
-            <li><RiMapPin2Fill /> Lorem ipsum dolor sit amet</li>
-            <li><LuAlarmClock /> Mon - Fri: 9:00 AM - 6:00 PM</li>
-            <li><LuAlarmClock /> Sat: 10:00 AM - 4:00 PM</li>
-            <li><FaTrain /> Near Central Station</li>
+            <li><RiMapPin2Fill size={25}/> Lorem ipsum dolor sit amet</li>
+            <li><LuAlarmClock size={25}/> Mon - Fri: 9:00 AM - 6:00 PM</li>
+            <li><LuAlarmClock size={25}/> Sat: 10:00 AM - 4:00 PM</li>
+            <li><FaTrain size={25}/> Near Central Station</li>
           </ul>
         </div>
       </div>
